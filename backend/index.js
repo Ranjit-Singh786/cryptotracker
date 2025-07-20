edd8328f4ff77bb4bbbe98172672691ade897623
ranjit <ranjit94cf@gmail.com>
2025-07-20 08:29:35 +0530

    added cron

--- backend/index.js
+++ backend/index.js
@@ -23,15 +23,21 @@ mongoose.connect(process.env.MONGODB_URI,
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 app.use('/api/crypto', require('./routes/cryptoRoutes'));
+app.get('/api/cron', handler);
+
+async function handler(req, res) {
+   console.log('Running hourly crypto data update...');
+  await fetchAndStoreCryptoData();
+}
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Crypto Tracker API');
 });
 
-cron.schedule('0 * * * *', () => {
-  console.log('Running hourly crypto data update...');
-  fetchAndStoreCryptoData();
-});
+// cron.schedule('0 * * * *', () => {
+//   console.log('Running hourly crypto data update...');
+//   fetchAndStoreCryptoData();
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
